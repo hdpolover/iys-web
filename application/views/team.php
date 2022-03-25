18f0a39b9e7f8f52d90830b4bf8be9a623c4c8f3
devisap <ilham.sagitaputra@gmail.com>
2022-03-26 03:41:12 +0700

    our tema

--- application/views/team.php
+++ application/views/team.php
@@ -1,16 +1,130 @@
 <main id="content" role="main">
 <!-- Hero Section -->
-<div class="d-flex">
-    <div class="container d-flex align-items-center vh-100 mt-5">
-    <div class="w-sm-75 w-lg-50 text-center mx-sm-auto">
-        <div class="mb-7">
-        <img class="img-fluid" src="<?= site_url()?>assets/svg/illustrations/oc-maintenance.svg" alt="SVG Illustration">
-        </div>
+<div class="container content-space-2 content-space-lg-3">
+    <!-- Heading -->
+    <div class="w-md-80 w-lg-50 text-center mx-md-auto mb-5 mb-md-9">
+    <!-- <span class="d-block small font-weight-bold text-cap mb-2">Our Team</span> -->
+    <h2>Our Team</h2>
+    </div>
+    <!-- End Heading -->
 
-        <h1 class="h2">We're just tuning up a few things.</h1>
-        <p>We apologize for the inconvenience but IYS Web is currently undergoing planned maintenance. Stay tuned!</p>
+    <div class="row mb-7">
+        <div class="col-sm-6 col-md-3 mb-5 mb-lg-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_1.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Qoriah Indah S </h4>
+                <p class="card-text">Website Developer</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3 mb-5 mb-lg-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_2.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Suhendra</h4>
+                <p class="card-text">Mobile Developer</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3 mb-5 mb-sm-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_3.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Muhamad Taufiq</h4>
+                <p class="card-text">Videographer</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_4.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Laras Wintang Kirana</h4>
+                <p class="card-text">Public Relation</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+        </div>
+        <!-- End Row -->
+
+        <!-- Card Info -->
+        <div class="text-center">
     </div>
+    <div class="row mb-7">
+        <div class="col-sm-6 col-md-3 mb-5 mb-lg-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_5.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Tazqia Ranyanisha Ananto</h4>
+                <p class="card-text">Content Manager</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3 mb-5 mb-lg-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_6.jpeg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Raisya Azzahra </h4>
+                <p class="card-text">Sponsorship</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3 mb-5 mb-sm-0">
+            <!-- Team -->
+            <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_7.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Aisyatus Syarifah Hanum </h4>
+                <p class="card-text">Secretary</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-md-3">
+            <!-- Team -->
+        <img class="card-img rounded-2" src="<?= site_url('assets/img/team/team_8.jpg')?>" alt="Image Description">
+            <div class="card card-sm text-center mt-n7 mx-3">
+            <div class="card-body">
+                <h4 class="card-title">Muhammad Ridho Al Fajri </h4>
+                <p class="card-text">Videographer</p>
+            </div>
+            </div>
+            <!-- End Team -->
+        </div>
+        <!-- End Col -->
+        </div>
+        <!-- End Row -->
+
+        <!-- Card Info -->
+        <div class="text-center">
     </div>
+    <!-- End Card Info -->
 </div>
 <!-- End Hero Section -->
 </main>
