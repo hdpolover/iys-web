2fc9206196991123f848a60c710a589e21a0693f
devisap <ilham.sagitaputra@gmail.com>
2022-07-23 13:03:20 +0700

    add google analytic

--- application/views/templates/usr/header.php
+++ application/views/templates/usr/header.php
@@ -27,6 +27,13 @@
   <!-- CSS Front Template -->
   <link rel="stylesheet" href="<?= site_url()?>assets/css/theme.min.css">
   <script src="<?= site_url()?>assets/js/jquery-3.6.0.min.js"></script>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-K3NCH5W');</script>
+  <!-- End Google Tag Manager -->
   <style>
     .maxText{
       display: -webkit-box;
